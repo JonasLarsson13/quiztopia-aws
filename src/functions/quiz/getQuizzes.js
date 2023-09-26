e801c5700ff1a3e24de883847a790e503967a640
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-09-26 11:20:32 +0200

    can now get all quizzes

--- src/functions/quiz/getQuizzes.js
+++ src/functions/quiz/getQuizzes.js
@@ -1,11 +1,29 @@
 import { sendResponse } from "../../responses/index.js";
+import { db } from "../../services/db.js";
 
-export const handler = async (event) => {
+export const handler = async () => {
   try {
+    const params = {
+      TableName: "Quizzes",
+      ProjectionExpression: "#quizId, #createdBy, #name",
+      ExpressionAttributeNames: {
+        "#quizId": "quizId",
+        "#createdBy": "createdBy",
+        "#name": "name",
+      },
+    };
+
+    const result = await db.scan(params).promise();
+
+    const quizzes = result.Items || [];
+
     return sendResponse(200, {
       success: true,
+      quizzes: quizzes,
     });
   } catch (error) {
-    return sendResponse(400, { error: "something went wrong" });
+    let message =
+      error?.details?.message || error?.message || "Something went wrong";
+    return sendResponse(400, { error: message });
   }
 };
