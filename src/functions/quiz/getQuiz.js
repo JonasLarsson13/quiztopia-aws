80258e1c48fde0392a901a894541eaa4fd23254a
Jonas Larsson <larsson2p0@MacBook-Pro-som-tillhor-Jonas.local>
2023-09-26 11:40:36 +0200

    get one quiz with all questions

--- src/functions/quiz/getQuiz.js
+++ src/functions/quiz/getQuiz.js
@@ -1,11 +1,58 @@
 import { sendResponse } from "../../responses/index.js";
+import { db } from "../../services/db.js";
 
 export const handler = async (event) => {
   try {
+    if (!event.pathParameters || !event.pathParameters.quizId) {
+      return sendResponse(400, {
+        error: "Missing quizId in query parameters",
+        event: event,
+      });
+    }
+    const quizId = event.pathParameters.quizId;
+    const createdBy = event.pathParameters.createdBy;
+
+    const quizParams = {
+      TableName: "Quizzes",
+      KeyConditionExpression: "quizId = :quizId",
+      ExpressionAttributeValues: {
+        ":quizId": quizId,
+      },
+    };
+
+    const quizResult = await db.query(quizParams).promise();
+
+    if (!quizResult.Items || quizResult.Items.length === 0) {
+      return sendResponse(404, { error: "Quiz not found" });
+    }
+
+    const questionParams = {
+      TableName: "Questions",
+      KeyConditionExpression: "createdBy = :createdBy AND quizId = :quizId",
+      ExpressionAttributeValues: {
+        ":createdBy": createdBy,
+        ":quizId": quizId,
+      },
+    };
+
+    const questionResult = await db.query(questionParams).promise();
+
+    const quizDetails = quizResult.Items[0];
+    const questions = questionResult.Items || [];
+
     return sendResponse(200, {
       success: true,
+      quiz: {
+        name: quizDetails.name,
+        questions: questions.map((question) => ({
+          question: question.question,
+          answer: question.answer,
+        })),
+      },
     });
   } catch (error) {
-    return sendResponse(400, { error: "something went wrong" });
+    let message =
+      error?.details?.message || error?.message || "Something went wrong";
+    return sendResponse(400, { error: message });
   }
 };
